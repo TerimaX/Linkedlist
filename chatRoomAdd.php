d30897edaf20d4124569f765f4768918d57e8f5b
TerimaX <64827131+TerimaX@users.noreply.github.com>
2022-06-22 11:36:29 +0900

    Add files via upload

--- chatRoomAdd.php
+++ chatRoomAdd.php
@@ -4,29 +4,45 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "roommating";
+$dbname = "android";
 
 // connect with database demo
 $conn = new mysqli($servername, $username, $password, $dbname);
 
  // an array to display response
- $response = array();
+
+ $isAlready = false;
 
  // on below line we are checking if the parameter send is id or not.
- if(isset($_POST['KakaoID1'], $_POST['KakaoID2'])){
-     // if the parameter send from the user id id then
-     // we will search the item for specific id.
+    if(isset($_POST['KakaoID1'], $_POST['KakaoID2'])){
+        // if the parameter send from the user id id then
+        // we will search the item for specific id.
 
-     $inputKakaoIDMe = $_POST['KakaoID1'];
-     $inputKakaoIDOther = $_POST['KakaoID1'];
- }
-        //on below line we are selecting the course detail with below id.
-   $stmt = $conn->prepare("INSERT INTO chatroom_tbl(KakaoId1, KakaoId2, Matched) values(?, ?, ?)");
-   $stmt->bind_param("iii", $inputKakaoIDMe, $inputKakaoIDOther, 1);
+        $inputKakaoIDMe = $_POST['KakaoID1'];
+        $inputKakaoIDOther = $_POST['KakaoID1'];
+
+        $stmt = $this->dbCon->prepare("SELECT * FROM chatroom_tbl where KakaoId1 = ?");
+            $stmt->bind_param("i",$inputKakaoIDMe);
+            $stmt->execute();
+            $stmt->bind_result($kakaoid1, $kakaoid2, $match);
+    }
+    while($stmt->fetch()){
+        if($kakaoid2 == $inputKakaoIDOther)
+            $isAlready = true;
+    }
+     
+    //on below line we are selecting the course detail with below id.
+   if($isAlready){
+        $response['error'] = true;
+        $response['message'] = "Already Matched!";
+   }
+   else{
+    $stmt = $conn->prepare("INSERT INTO chatroom_tbl(KakaoId1, KakaoId2, Matched) values(?, ?, 1)");
+   $stmt->bind_param("ii", $inputKakaoIDMe, $inputKakaoIDOther);
    $result = $stmt->execute();
 
-   $stmt = $conn->prepare("INSERT INTO chatroom_tbl(KakaoId1, KakaoId2, Matched) values(?, ?, ?)");
-   $stmt->bind_param("iii",$inputKakaoIDOther, $inputKakaoIDMe, 1);
+   $stmt = $conn->prepare("INSERT INTO chatroom_tbl(KakaoId1, KakaoId2, Matched) values(?, ?, 1)");
+   $stmt->bind_param("ii",$inputKakaoIDOther, $inputKakaoIDMe);
    $result = $stmt->execute();
    // on below line we are checking if our
    // table is having data with specific id.
@@ -42,6 +58,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
          $response['error'] = true;
          $response['message'] = "Incorrect id";
      }
+    }
 
  // at last we are printing
  // all the data on below line.
